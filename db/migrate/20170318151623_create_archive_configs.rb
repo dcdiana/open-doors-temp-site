a59e3eafa07143bc9a104b6fd28d8676792af55a
Ariana <ariana@astele.co.uk>
2017-04-10 20:09:03 +0100

    OD-119 Read config from yaml file in migration

--- db/migrate/20170318151623_create_archive_configs.rb
+++ db/migrate/20170318151623_create_archive_configs.rb
@@ -1,18 +1,21 @@
 class CreateArchiveConfigs < ActiveRecord::Migration[5.0]
   def change
-    ActiveRecord::Base.establish_connection(Rails.Application::CONFIG_DB_CONF)
+    db_conf = YAML.load(ERB.new(File.read("config/database.yml")).result)
+
+    config = db_conf["config_db"][Rails.env]
+    ActiveRecord::Base.establish_connection(config)
 
     create_table :archive_configs, id: :integer, default: 0, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
       t.string "key", limit: 45, null: false
-      t.string "name", limit: 255, default: null
-      t.string "fandom", limit: 255, default: null
+      t.string "name", limit: 255
+      t.string "fandom", limit: 255
       t.string "stories_note", limit: 65535
       t.string "bookmarks_note", limit: 65535
       t.boolean "send_email", default: false, null: false
       t.boolean "post_preview", default: false, null: false
       t.integer "items_per_page", default: 30, null: false
       t.string "archivist", limit: 255, default: "testy", null: false
-      t.string "collection_name", limit: 255, default: null
+      t.string "collection_name", limit: 255
       t.integer "imported", default: 0
       t.integer "notImported", default: 0
       t.index ["id"], name: "id_UNIQUE", unique: true, using: :btree
